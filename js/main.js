9550ad86d2bc10bb1c4035cd005e90a77e651875
MARCELO AKIRA <marcelo.yasunici@gmail.com>
2024-06-18 13:14:15 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false
+    });
+
+
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('#cpf').mask('000.000.000-00');
+
+    $('#cep').mask('00000-000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+        },
+
+    submitHandler: function() {
+        alert(`Sua requisição foi enviada para análise, parabéns pela aquisição!`);
+        form.reset();
+    },
+
+    invalidHandler: function(evento, validator) {
+        let camposIncorretos = validator.numberOfInvalids();
+        if(camposIncorretos) {
+            alert(`Por favor, preencha os campos para prosseguir com a compra!`);
+        }
+    }
+    })
+})
